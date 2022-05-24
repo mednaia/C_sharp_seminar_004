8fc99b9ab262f63cd45d399c18af7c47b295d0c2
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-24 10:57:28 +0300

    Task 001 change numbers of  array

---
+++ Task001_change/Program.cs
@@ -0,0 +1,41 @@
+﻿// Написать программу замену элементов массива на противоположные
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-10,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] array = new int[10];
+
+
+void ChangeArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]*-1} ");
+    }
+     Console.WriteLine();
+}
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
