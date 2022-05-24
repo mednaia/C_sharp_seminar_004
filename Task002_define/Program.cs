4a9ef046e268c9e83fc16174cd8a726a963d4861
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-24 11:22:04 +0300

    Task 002 searching of number in array

---
+++ Task002_define/Program.cs
@@ -0,0 +1,39 @@
+﻿// Определить, присутствует ли в заданном массиве, некоторое число
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-10,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void searchInArray(int[] array)
+{
+    Console.WriteLine("Input integer and press Enter: ");
+    int searchNumber = int.Parse(Console.ReadLine());
+    for (int j = 0; j < array.Length; j++)
+        {
+            if(array[j] == searchNumber) Console.Write($"The number {searchNumber} is in the array on position {j}. ");;
+        }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+searchInArray(array);
