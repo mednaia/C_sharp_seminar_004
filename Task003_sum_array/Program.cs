5d132caea5b1136d49cbbbdde446b520b6eaedba
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-25 13:43:02 +0300

    task 003 sum of odd elements in array

--- Task003_sum_array/Program.cs
+++ Task003_sum_array/Program.cs
@@ -1,42 +1,11 @@
 ﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
-
-// Найти сумму чисел одномерного массива стоящих на нечетной позиции
-void FillArray(int[] collection)
+int[]array = new int[8];
+for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(-10,11);
+int sum = 0;
+for(int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+for (int i = 1; i < array.Length; i = i+2) 
 {
-    int length = collection.Length;
-    int index = 0;
-    while (index < length)
-    {
-        collection[index] = new Random().Next(-10,10);
-        index++;
-    }
+    sum+=array[i];
 }
-
-void PrintArray(int[] array)
-{
-    int count = array.Length;
-
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
-
-void SumOdd(int[] array)
-{
-    int count = array.Length;
-    int sum = 0;
-    for(int i = 0; i < count; i++)
-    {
-        if (i == 1 || i%2 == 1) sum+=array[i];
-    }
-    Console.Write($"{sum} ");
-}
-
-int[] array = new int[6];
-
-FillArray(array);
-PrintArray(array);
 Console.WriteLine();
-SumOdd(array);
+Console.WriteLine($"The sum of odd-elemets in array = {sum} ");
