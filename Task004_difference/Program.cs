48def2be34e347f04903038e2435f64f838c7aeb
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-25 14:23:32 +0300

    task004 difference between max and min elements

---
+++ Task004_difference/Program.cs
@@ -0,0 +1,6 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+int[]array = new int[8];
+for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(-10,11);
+for(int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+Console.WriteLine();
+Console.WriteLine($"The difference between the maximum and minimum elements {array[array.Length-1]-array[0]}");
