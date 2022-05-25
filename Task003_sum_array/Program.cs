55320e893db37b410deb8b694f066e5375ea5c17
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-25 13:24:12 +0300

    ask003 sum numbers, which stay on odd position in array

---
+++ Task003_sum_array/Program.cs
@@ -0,0 +1,43 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-10,10);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SumOdd(int[] array)
+{
+    int count = array.Length;
+    int sum = 0;
+    for(int i = 0; i < count; i++)
+    {
+        if (i == 1 || i%2 == 1)
+        sum = sum + array[i];
+    }
+    Console.Write($"{sum} ");
+}
+
+int[] array = new int[6];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+SumOdd(array);
