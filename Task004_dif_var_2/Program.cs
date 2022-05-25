7da3a2648d06aad7d595c50c8ae16bf9bcc07b95
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-25 14:24:34 +0300

    task 004 difference between max and min numbers in array

---
+++ Task004_dif_var_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+int[]array = new int[8];
+for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(-10,11);
+for(int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+int minPosition = 0;
+int maxPosition = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < array[minPosition]) minPosition = i;
+    }
+for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > array[maxPosition]) maxPosition = i;
+    }
+Console.WriteLine();
+Console.WriteLine($"The maximum elements = {array[maxPosition]} on position number {maxPosition}");
+Console.WriteLine($"The minimum elements = {array[minPosition]} on position number {minPosition}");
+Console.WriteLine($"The difference between the maximum and minimum elements {array[maxPosition]-array[minPosition]}");
