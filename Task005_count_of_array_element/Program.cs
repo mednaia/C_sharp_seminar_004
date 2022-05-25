c0d12ee25901ceeeac269f2ebc65fa6d7cbfdbcb
Ekaterina Tyurina <tiurina.kate@gmail.com>
2022-05-25 14:38:16 +0300

    task005 count of array element

---
+++ Task005_count_of_array_element/Program.cs
@@ -0,0 +1,16 @@
+﻿// Посчитать количество вхождений элемента в массив
+int[]array = new int[8];
+for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(-10,11);
+for(int i = 0; i < array.Length; i++) Console.Write($"{array[i]} ");
+Console.WriteLine();
+Console.WriteLine("Input integer and press Enter: ");
+    int searchNumber = int.Parse(Console.ReadLine());
+    int count =0;
+    for (int i = 0; i < array.Length; i++)
+        {
+            if(array[i] == searchNumber)
+            {
+                count++;
+            }
+        }
+Console.WriteLine($"The number {searchNumber} is found in array {count} times.");
